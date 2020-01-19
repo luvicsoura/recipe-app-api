cfa8ff0ba13fc87853fe4cc9b22bce4d6baa2036
Victor Oliveira <kirith@pop-os.localdomain>
2020-01-19 17:57:01 -0300

    Fix linting errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
 
         if not email:
             raise ValueError('Users must have an email address')
-        
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -25,12 +27,11 @@ class UserManager(BaseUserManager):
         return user
 
 
-
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
 
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
+    email = models.EmailField(max_length=255, unique = True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default = False)
 
